016080f97666f9bf14b90db0167423774b265aec
shafrinahamed097 <shafrinahamedsa@gmail.com>
2025-07-02 22:47:34 -0600

    setup dart basic

---
+++ cls_2/bin/cls_2.dart
@@ -0,0 +1,5 @@
+import 'package:cls_2/cls_2.dart' as cls_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cls_2.calculate()}!');
+}
---
+++ cls_2/test/cls_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:cls_2/cls_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
